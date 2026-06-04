67226e8ec2e8b63c71f99d02b87c68f423343e1f
iron5050 <benedikt.ingi.a@gmail.com>
2026-06-04 09:04:49 +0000

    play controller push 1

--- Assets/Scripts/PlayerController/PlayerController.cs
+++ Assets/Scripts/PlayerController/PlayerController.cs
@@ -2,15 +2,22 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public CharacterController Controler;
+
+    //charachter speed
+    public float speed = 6f;
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            Controler.Move(direction * speed * Time.deltaTime);
+        }
     }
 }
