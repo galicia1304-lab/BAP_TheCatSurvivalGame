9b52f8f7991980b969dc861662e1e8196fd37c44
iron5050 <benedikt.ingi.a@gmail.com>
2026-06-10 06:35:23 +0000

    3rd person camera (pause)

---
+++ Assets/Scripts/PlayerController/PlayerScript.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class PlayerScript : MonoBehaviour
+{
+
+    public InputAction InputActions;
+    public CharacterController playerChatachterController;
+
+
+    private void OnEnable()
+    {
+        InputActions.FindActionMap("Player").Enable();
+    }
+}
